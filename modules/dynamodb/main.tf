5c95842ccea7347298ab16d65c89c3b815a26fd7
Shashidara G <shashi2668g@gmail.com>
2024-09-25 19:26:53 +0530

    Added modules for S3 bucket and DynamoDB table

---
+++ modules/dynamodb/main.tf
@@ -0,0 +1,13 @@
+resource "aws_dynamodb_table" "new" {
+  name         = var.table_name
+  billing_mode = var.billing_mode
+
+  hash_key = var.hash_key
+
+  attribute {
+    name = var.hash_key
+    type = var.hash_key_type
+  }
+
+  tags = var.tags
+}
